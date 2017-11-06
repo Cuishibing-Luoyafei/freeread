c6c4b680f8da131a519dd239426777b519502ed5
cui shibing <643237029@qq.com>
2017-11-06 23:51:45 +0800

    开始搭建redis缓存环境

--- src/main/java/cui/shibing/freeread/app/LayoutController.java
+++ src/main/java/cui/shibing/freeread/app/LayoutController.java
@@ -1,5 +1,7 @@
 package cui.shibing.freeread.app;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ public class LayoutController {
 
 	@RequestMapping(value = "/novelDeatil", params = "novelId")
 	public String novelDetails(Model model) {
+		CacheManager a = new RedisCacheManager();
 		return "pageElement/novelDeatil.no_left_layout";
 	}
 
