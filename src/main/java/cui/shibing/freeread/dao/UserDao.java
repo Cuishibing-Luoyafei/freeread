67609b29193d7e7242d6f5d873abd5a654a0d3ac
cui shibing <643237029@qq.com>
2017-11-18 00:14:25 +0800

    实现UserDao接口

--- src/main/java/cui/shibing/freeread/dao/UserDao.java
+++ src/main/java/cui/shibing/freeread/dao/UserDao.java
@@ -4,7 +4,6 @@ import cui.shibing.freeread.model.User;
 
 public interface UserDao {
     int insertUser(User user);
-    
     int deleteUserById(String userId);
     User selectByUserId(String userId);
     int updateUser(User user);
--- src/test/java/cui/shibing/freeread/dao/UserDaoTest.java
+++ src/test/java/cui/shibing/freeread/dao/UserDaoTest.java
@@ -23,4 +23,19 @@ public class UserDaoTest extends CustomDaoTest{
         user.setUserInfoId(UUID.randomUUID().toString());
         assertTrue(userDao.insertUser(user)==1);
     }
+
+    @Test
+    public void testDeleteUser(){
+        User user = new User();
+        String userId = UUID.randomUUID().toString();
+        user.setUserId(userId);
+        user.setUserName(UUID.randomUUID().toString());
+        user.setUserPass(UUID.randomUUID().toString());
+        user.setUserRole(1);
+        user.setUserInfoId(UUID.randomUUID().toString());
+        userDao.insertUser(user);
+        assertTrue(userDao.selectByUserId(userId)!=null);
+        assertTrue(userDao.deleteUserById(userId) == 1);
+        assertTrue(userDao.selectByUserId(userId)==null);
+    }
 }
