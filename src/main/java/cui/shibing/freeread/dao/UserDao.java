bd507e02230beead118dcd88a4529588ef6f334e
cui shibing <643237029@qq.com>
2017-11-17 23:51:07 +0800

    add UserDao

--- src/main/java/cui/shibing/freeread/dao/UserDao.java
+++ src/main/java/cui/shibing/freeread/dao/UserDao.java
@@ -4,6 +4,7 @@ import cui.shibing.freeread.model.User;
 
 public interface UserDao {
     int insertUser(User user);
+    
     int deleteUserById(String userId);
     User selectByUserId(String userId);
     int updateUser(User user);
