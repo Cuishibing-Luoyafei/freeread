48282f73676b8ce7109483b56117439a84c41b88
cui shibing <643237029@qq.com>
2017-11-21 21:31:45 +0800

    完成UserService

--- src/main/java/cui/shibing/freeread/service/UserService.java
+++ src/main/java/cui/shibing/freeread/service/UserService.java
@@ -3,8 +3,7 @@ package cui.shibing.freeread.service;
 import cui.shibing.freeread.model.User;
 
 public interface UserService {
-    User getUserById(String userId);
     User getUserByName(String userName);
-    int deleteUserById(String userId);
-    int updateUserById(User user);
+    int deleteUserByName(String userName);
+    int updateUserByName(User user);
 }
--- src/main/java/cui/shibing/freeread/service/impl/UserServiceImpl.java
+++ src/main/java/cui/shibing/freeread/service/impl/UserServiceImpl.java
@@ -4,33 +4,36 @@ import cui.shibing.freeread.dao.UserDao;
 import cui.shibing.freeread.model.User;
 import cui.shibing.freeread.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
 
     @Override
-    public User getUserById(String userId) {
-        if(!StringUtils.isEmpty(userId)){
-            return userDao.selectByUserId(userId);
+    public User getUserByName(String userName) {
+        if(!StringUtils.isEmpty(userName)){
+            return userDao.selectByUserName(userName);
         }
         return null;
     }
 
     @Override
-    public User getUserByName(String userName) {
-        return null;
-    }
-
-    @Override
-    public int deleteUserById(String userId) {
+    public int deleteUserByName(String userName) {
+        if(!StringUtils.isEmpty(userName)){
+            return userDao.deleteUserByName(userName);
+        }
         return 0;
     }
 
     @Override
-    public int updateUserById(User user) {
+    public int updateUserByName(User user) {
+        if(user != null && !StringUtils.isEmpty(user.getUserName())){
+            return userDao.updateUser(user);
+        }
         return 0;
     }
 }
