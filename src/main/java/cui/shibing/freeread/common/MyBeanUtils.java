d6273ed4fba0bfcb629ebb6fee3b271a2826ca53
Cuishibing <643237029@qq.com>
2018-04-19 09:35:07 +0800

    拷贝失败时抛出异常，参数判空，增加返回值

--- src/main/java/cui/shibing/freeread/common/MyBeanUtils.java
+++ src/main/java/cui/shibing/freeread/common/MyBeanUtils.java
@@ -4,7 +4,13 @@ import org.springframework.beans.BeanUtils;
 
 import java.util.List;
 
+import java.lang.RuntimeException;
+
+import java.util.logging.Logger;
+
 public class MyBeanUtils extends BeanUtils {
+    
+    private static Logger logger = Logger.getLogger(MyBeanUtils.class);
 
     /**
      * 复制List对象,对源List中的对象依次执行copyProperties操作
@@ -13,18 +19,20 @@ public class MyBeanUtils extends BeanUtils {
      * @param target         目标List对象
      * @param targetEleClass List中对象的Class对象
      */
-    public static <T, E> void copyListProperties(List<T> source, List<E> target, Class<E> targetEleClass) {
-        if (source == null || target == null)
-            return;
+    public static <T, E> boolean copyListProperties(List<T> source, List<E> target, Class<E> targetEleClass) {
+        if (source == null || target == null || targetEleClass == null){
+            logger.warning("copy failed");
+            return false;
+        }
         for (T s : source) {
             try {
                 E t = targetEleClass.newInstance();
                 copyProperties(s, t);
                 target.add(t);
             } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-                target.clear();
+                throw new RuntimeException(e);
             }
         }
+        return true;
     }
 }
