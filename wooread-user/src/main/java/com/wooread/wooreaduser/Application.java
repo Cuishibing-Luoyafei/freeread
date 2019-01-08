9185314b9313b52908357c96dd6a6627289b6e57
Cui shibing <643237029@qq.com>
2019-01-08 17:48:21 +0800

    eureka server

--- wooread-user/src/main/java/com/wooread/wooreaduser/Application.java
+++ wooread-user/src/main/java/com/wooread/wooreaduser/Application.java
@@ -3,6 +3,7 @@ package com.wooread.wooreaduser;
 import cui.shibing.EnableCommonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCommonRepository
+@EnableEurekaClient
 @RestController
 public class Application {
     public static void main(String[] args) {
