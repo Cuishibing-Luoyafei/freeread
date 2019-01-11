8e2810ca6cae7043661de9045ff0eae013b5cb4f
Cui shibing <643237029@qq.com>
2019-01-11 11:22:51 +0800

    添加注释

--- wooread-novel/src/main/java/com/wooread/wooreadnovel/service/UserService.java
+++ wooread-novel/src/main/java/com/wooread/wooreadnovel/service/UserService.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * wooread-user微服务暴露出的接口
+ */
 @FeignClient(value = "wooread-user")
 public interface UserService {
 
